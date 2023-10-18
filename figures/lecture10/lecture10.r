f14eca732d19b9fb808d80022ef80eb91c242bfe
David Ubilava <david.ubilava@sydney.edu.au>
2023-10-18 21:32:27 +1100

    lecture 10

--- figures/lecture10/lecture10.r
+++ figures/lecture10/lecture10.r
@@ -1,36 +1,21 @@
 # load the libraries (install them if needed)
 library(data.table)
 library(ggplot2)
-# library(camcorder)
 library(extrafont)
 # font_import()
 loadfonts(device="win",quiet=T)
 library(cowplot)
-library(crypto2)
 library(fredr)
 fredr_set_key("7a1db535f59c2ac4382b9c22a15b5f06")
 library(stringr)
-# library(readabs)
-# Sys.setenv(R_READABS_PATH = "figures/lecture5")
 library(lmtest)
 library(sandwich)
 library(forecast)
-
+library(vars)
 library(fastDummies)
 
 rm(list=ls())
 gc()
-
-# all_wpi <- read_abs("6345.0")
-
-# # camcorder stuff
-# camcorder::gg_record(
-#   dir='figures/lecture4',
-#   width=6.5,
-#   height=6.5*9/16,
-#   dpi=300,
-#   bg="white"
-# )
 
 # plot aesthetics
 theme_eg <- function(base_size=12,base_family="Segoe Print",border=F){
@@ -41,9 +26,10 @@ theme_eg <- function(base_size=12,base_family="Segoe Print",border=F){
     panel.grid.minor=element_blank(),
     plot.background=element_rect(fill="white",color=NA),
     plot.title=element_text(family=base_family,size=rel(1.2),colour="dimgray"),
-    plot.caption=element_text(family=base_family,colour="darkgray"),
+    plot.subtitle=element_text(family=base_family,size=rel(1.2),colour="dimgray"),
+    plot.caption=element_text(family="Segoe UI",colour="darkgray"),
     plot.margin=margin(.25,.25,.25,.25,"lines"),
-    axis.title=element_text(family=base_family,face="bold",size=rel(1.3),colour="dimgray"),
+    axis.title=element_text(family=base_family,face="bold",size=rel(1.2),colour="dimgray"),
     axis.text=element_text(family=base_family,size=rel(1.1),colour="dimgray",margin=margin(t=1,r=1,b=1,l=1)),
     axis.line=element_blank(),
     axis.ticks=element_blank(),
@@ -57,203 +43,269 @@ theme_eg <- function(base_size=12,base_family="Segoe Print",border=F){
   )
 }
 
-# inflation series ----
-inflation_dt <- data.table(fredr(series_id="CPIAUCNS",observation_start=as.Date("1970-01-01"),observation_end=as.Date("2022-12-31"),frequency="m",units="pc1"))
+# crude oil price series ----
+crudeoil_dt <- data.table(fredr(series_id="DCOILWTICO",observation_start=as.Date("2004-01-01"),observation_end=as.Date("2022-12-31"),frequency="m",units="lin"))
 
-save(inflation_dt,file="figures/lecture9/inflation.RData")
+save(crudeoil_dt,file="figures/lecture10/crudeoil.RData")
+
+# 5-yr expected inflation ----
+inflation_dt <- data.table(fredr(series_id="T5YIE",observation_start=as.Date("2004-01-01"),observation_end=as.Date("2022-12-31"),frequency="m",units="lin"))
+
+save(inflation_dt,file="figures/lecture10/inflation.RData")
+
+dt <- merge(inflation_dt[,.(date,infl=value)],crudeoil_dt[,.(date,coil=value)],by="date")
+
+dl <- melt(dt,id.vars="date")
+  
+dl$variable <- factor(dl$variable,levels=c("coil","infl"),labels=c("Crude Oil Prices","5-Year Expected Inflation"))
+  
+gg1 <- ggplot(dt,aes(x=date,y=coil))+
+  geom_line(color="dimgray",size=.6)+
+  labs(y="",x="Year",title="Crude Oil Prices - WTI",subtitle="($/bbl)")+
+  coord_cartesian(ylim=c(0,150),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+
+gg2 <- ggplot(dt,aes(x=date,y=infl))+
+  geom_line(color="coral",size=.6)+
+  labs(y="",x="Year",title="5-yr Expected Inflation",subtitle="(%)")+
+  coord_cartesian(ylim=c(-2,4),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+
+gg <- plot_grid(gg1,gg2,align="hv",axis="tb",nrow=1)
+
+ggsave("figures/lecture10/series.png",gg,width=6.5,height=6.5*8/16,dpi="retina",device="png")
+  
+
+dt[,`:=`(coil1=shift(coil,1),coil2=shift(coil,2),coil3=shift(coil,3),coil4=shift(coil,4),infl1=shift(infl,1),infl2=shift(infl,2),infl3=shift(infl,3),infl4=shift(infl,4))]
+    
+var11 <- lm(coil~coil1+infl1,data=dt)
+var12 <- lm(infl~coil1+infl1,data=dt)
+
+var21 <- lm(coil~coil1+coil2+infl1+infl2,data=dt)
+var22 <- lm(infl~coil1+coil2+infl1+infl2,data=dt)
+
+var31 <- lm(coil~coil1+coil2+coil3+infl1+infl2+infl3,data=dt)
+var32 <- lm(infl~coil1+coil2+coil3+infl1+infl2+infl3,data=dt)
+
+var41 <- lm(coil~coil1+coil2+coil3+coil4+infl1+infl2+infl3+infl4,data=dt)
+var42 <- lm(infl~coil1+coil2+coil3+coil4+infl1+infl2+infl3+infl4,data=dt)
+    
+    
+p <- 1
+k <- 2
+var1r <- cbind(var11$residuals,var12$residuals)
+cov1r <- crossprod(var1r)/(nrow(dt)-(p*k^2+k))
+
+AIC1 <- log(det(cov1r))+2*(p*k^2+k)/nrow(dt)
+
+
+p <- 2
+k <- 2
+var2r <- cbind(var21$residuals,var22$residuals)
+cov2r <- crossprod(var2r)/(nrow(dt)-(p*k^2+k))
+
+AIC2 <- log(det(cov2r))+2*(p*k^2+k)/nrow(dt)
+
+
+p <- 3
+k <- 2
+var3r <- cbind(var31$residuals,var32$residuals)
+cov3r <- crossprod(var3r)/(nrow(dt)-(p*k^2+k))
+
+AIC3 <- log(det(cov3r))+2*(p*k^2+k)/nrow(dt)
+
+
+p <- 4
+k <- 2
+var4r <- cbind(var41$residuals,var42$residuals)
+cov4r <- crossprod(var4r)/(nrow(dt)-(p*k^2+k))
+
+AIC4 <- log(det(cov4r))+2*(p*k^2+k)/nrow(dt)
+
+aic_dt <- data.table(p=1:4,AIC=NA)
+
+aic_dt$AIC[1] <- AIC1
+aic_dt$AIC[2] <- AIC2
+aic_dt$AIC[3] <- AIC3
+aic_dt$AIC[4] <- AIC4
 
 
 
 
-# U.S. Interest Rates and Inflation
 
-.right-column[
-  ```{r echo=FALSE, message=FALSE, cache=FALSE}
-  load("../../Books/forecasting/data/funds.RData")
-  load("../../Books/forecasting/data/inflation.RData")
-  
-  dt <- merge(funds,inflation,by="date")
-  colnames(dt) <- c("date","funds","inflation")
-  
-  dl <- melt(dt,id.vars="date")
-  
-  dl$variable <- factor(dl$variable,levels=c("funds","inflation"),labels=c("Federal Funds Rate","Year-Over-Year Inflation"))
-  
-  ggplot(dl,aes(x=date,y=value,color=variable,linetype=variable))+
-    geom_line(size=.8)+
-    scale_color_manual(values=c("powderblue","coral"))+
-    scale_linetype_manual(values=c(1,5))+
-    labs(x="Year",y="%",caption="retrieved from FRED, Federal Reserve Bank of St. Louis\nhttps://fred.stlouisfed.org/series/CPIAUCNS")+
-    theme_classic()+
-    theme(axis.title = element_text(size=22),axis.text = element_text(size=18),legend.position="top",legend.title=element_blank(),legend.text=element_text(size=18))
-  ```
-]
+# multistep: known x ----
 
----
-  
-  # U.S. Interest Rates and Inflation: VAR(3)
-  
-  .pull-left[
-    ```{r echo=FALSE, message=FALSE, cache=FALSE}
-    dt[,`:=`(funds1=shift(funds,1),funds2=shift(funds,2),funds3=shift(funds,3),funds4=shift(funds,4),inflation1=shift(inflation,1),inflation2=shift(inflation,2),inflation3=shift(inflation,3),inflation4=shift(inflation,4))]
-    
-    var11 <- lm(funds~funds1+inflation1,data=dt)
-    var12 <- lm(inflation~funds1+inflation1,data=dt)
-    
-    var21 <- lm(funds~funds1+funds2+inflation1+inflation2,data=dt)
-    var22 <- lm(inflation~funds1+funds2+inflation1+inflation2,data=dt)
-    
-    var31 <- lm(funds~funds1+funds2+funds3+inflation1+inflation2+inflation3,data=dt)
-    var32 <- lm(inflation~funds1+funds2+funds3+inflation1+inflation2+inflation3,data=dt)
-    
-    var41 <- lm(funds~funds1+funds2+funds3+funds4+inflation1+inflation2+inflation3+inflation4,data=dt)
-    var42 <- lm(inflation~funds1+funds2+funds3+funds4+inflation1+inflation2+inflation3+inflation4,data=dt)
-    
-    
-    p <- 1
-    k <- 2
-    var1r <- cbind(var11$residuals,var12$residuals)
-    cov1r <- crossprod(var1r)/(nrow(dt)-(p*k^2+k))
-    
-    AIC1 <- log(det(cov1r))+2*(p*k^2+k)/nrow(dt)
-    
-    
-    p <- 2
-    k <- 2
-    var2r <- cbind(var21$residuals,var22$residuals)
-    cov2r <- crossprod(var2r)/(nrow(dt)-(p*k^2+k))
-    
-    AIC2 <- log(det(cov2r))+2*(p*k^2+k)/nrow(dt)
-    
-    
-    p <- 3
-    k <- 2
-    var3r <- cbind(var31$residuals,var32$residuals)
-    cov3r <- crossprod(var3r)/(nrow(dt)-(p*k^2+k))
-    
-    AIC3 <- log(det(cov3r))+2*(p*k^2+k)/nrow(dt)
-    
-    
-    p <- 4
-    k <- 2
-    var4r <- cbind(var41$residuals,var42$residuals)
-    cov4r <- crossprod(var4r)/(nrow(dt)-(p*k^2+k))
-    
-    AIC4 <- log(det(cov4r))+2*(p*k^2+k)/nrow(dt)
-    
-    aic_dt <- data.table(p=1:4,AIC=NA)
-    
-    aic_dt$AIC[1] <- AIC1
-    aic_dt$AIC[2] <- AIC2
-    aic_dt$AIC[3] <- AIC3
-    aic_dt$AIC[4] <- AIC4
-    
-    knitr::kable(aic_dt,format='html',digits=3,align="l", table.attr='class="myTable"')
-    
-    ```
-  ]
+h1 <- as.Date("2016-01-01")
+h2 <- max(dt$date)
 
-.pull-right[
-  ```{r echo=FALSE, message=FALSE, cache=FALSE}
-  out1 <- t(summary(var31)$coef[,c(1,2)])
-  rownames(out1) <- c("estimate","s.e.")
-  
-  out2 <- t(summary(var32)$coef[,c(1,2)])
-  rownames(out2) <- c("estimate","s.e.")
-  
-  out <- rbind(out1,out2)
-  kable(out, booktabs = TRUE,digits=3,align="r", table.attr='class="myTable"',row.names = T,col.names = c("$\\alpha_i$","$\\pi_{i1}^{(1)}$","$\\pi_{i1}^{(2)}$","$\\pi_{i1}^{(3)}$","$\\pi_{i2}^{(1)}$","$\\pi_{i2}^{(2)}$","$\\pi_{i2}^{(3)}$"),escape = FALSE) %>% pack_rows(
-    index = c("Interest Rate (i=1)" = 2, "Inflation Rate (i=2)" = 2)
-  ) %>%
-    kable_styling(font_size = 22)
-  ```
-]
+R <- which(dt$date=="2015-12-01")
+P <- nrow(dt)-R
 
----
+dt[,`:=`(f_i=as.numeric(NA),f_il=as.numeric(NA),f_iu=as.numeric(NA))]
+
+# dt <- dt[-c(1:4)]
+
+# linear trend
+est <- lm(infl~infl1+infl2+coil+coil1+coil2,data=dt[date<h1])
+
+eps <- est$residuals
+
+B <- 5000
+
+boot_mat <- matrix(nrow=R+P,ncol=B)
+
+for(b in 1:B){
+  set.seed(b)
+  dt[,`:=`(yb=infl,eb=sample(eps,R+P,replace=T))]
+  
+  for(i in 1:P){
+    
+    dt$yb[R+i] <- est$coefficients["(Intercept)"]+est$coefficients["infl1"]*dt$yb[R+i-1]+est$coefficients["infl2"]*dt$yb[R+i-2]+est$coefficients["coil"]*dt$coil[R+i]+est$coefficients["coil1"]*dt$coil1[R+i]+est$coefficients["coil2"]*dt$coil2[R+i]+dt$eb[R+i]
+    
+  }
+  
+  boot_mat[,b] <- dt$yb
+}
+
+dt[,`:=`(y_f=infl,f=apply(boot_mat,1,mean),l=apply(boot_mat,1,quantile,.05),u=apply(boot_mat,1,quantile,.95))]
+dt$y_f[1:R] <- NA
+dt$f[1:R] <- NA
+dt$l[1:R] <- NA
+dt$u[1:R] <- NA
+
+
+
+gg_boot <- ggplot(dt,aes(x=date))+
+  geom_ribbon(aes(ymin=l,ymax=u),fill="coral",alpha=.2)+
+  geom_line(aes(y=infl),linewidth=.6,color="dimgray",na.rm=T)+
+  geom_line(aes(y=y_f),linewidth=.6,color="gray",na.rm=T)+
+  geom_line(aes(y=l),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=u),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=f),linetype=5,linewidth=.6,color="coral",na.rm=T)+
+  labs(x="Year",y="5-yr Expected Inflation (%)")+
+  coord_cartesian(ylim=c(-2,4),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+
+gg_boot
+
+ggsave("figures/lecture10/inflation_known.png",gg_boot,width=6.5,height=6.5*8/16,dpi="retina",device="png")
+
+
+boot_mat <- matrix(nrow=R+P,ncol=B)
+
+for(b in 1:B){
+  set.seed(b)
+  dt[,`:=`(yb=infl,xb=dt[date==h1]$coil,eb=sample(eps,R+P,replace=T))]
+  dt[date<h1,xb:=coil]
+  
+  for(i in 1:P){
+    
+    dt$yb[R+i] <- est$coefficients["(Intercept)"]+est$coefficients["infl1"]*dt$yb[R+i-1]+est$coefficients["infl2"]*dt$yb[R+i-2]+est$coefficients["coil"]*dt$xb[R+i]+est$coefficients["coil1"]*dt$xb[R+i-1]+est$coefficients["coil2"]*dt$xb[R+i]+dt$eb[R+i-2]
+    
+  }
+  
+  boot_mat[,b] <- dt$yb
+}
+
+dt[,`:=`(y_f=infl,f=apply(boot_mat,1,mean),l=apply(boot_mat,1,quantile,.05),u=apply(boot_mat,1,quantile,.95))]
+dt$y_f[1:R] <- NA
+dt$f[1:R] <- NA
+dt$l[1:R] <- NA
+dt$u[1:R] <- NA
+
+
+
+gg_boot <- ggplot(dt,aes(x=date))+
+  geom_ribbon(aes(ymin=l,ymax=u),fill="coral",alpha=.2)+
+  geom_line(aes(y=infl),linewidth=.6,color="dimgray",na.rm=T)+
+  geom_line(aes(y=y_f),linewidth=.6,color="gray",na.rm=T)+
+  geom_line(aes(y=l),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=u),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=f),linetype=5,linewidth=.6,color="coral",na.rm=T)+
+  labs(x="Year",y="5-yr Expected Inflation (%)")+
+  coord_cartesian(ylim=c(-2,4),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+
+gg_boot
+
+ggsave("figures/lecture10/inflation_fixed.png",gg_boot,width=6.5,height=6.5*8/16,dpi="retina",device="png")
+
+
+
+
+  
+
+ar21 <- lm(infl~infl1+infl2,data=dt)
+ar22 <- lm(coil~coil1+coil2,data=dt)
+
+var21 <- lm(infl~infl1+infl2+coil1+coil2,data=dt)
+var22 <- lm(coil~infl1+infl2+coil1+coil2,data=dt)
+
+f21 <- anova(var21,ar21)
+f22 <- anova(var22,ar22)
+
+out <- rbind(f21[2,c(5:6)],f22[2,c(5:6)])
+
+
+
+R <- which(dt$date=="2015-12-01")
+P <- nrow(dt)-R
+
+ts_dt <- ts(dt[1:R,.(coil,infl)],start=c(as.numeric(substr(dt$date[1],1,4)),as.numeric(substr(dt$date[1],6,7))),frequency = 12)
+
+var3 <- VAR(ts_dt,p=3,type="const")
+
+for3 <- forecast::forecast(var3,level=95,h=P)
+
+
+dt$f.coil <- NA
+dt$l.coil <- NA
+dt$u.coil <- NA
+
+dt$f.infl <- NA
+dt$l.infl <- NA
+dt$u.infl <- NA
+
+dt$f.coil[(R+1):(R+P)] <- for3$forecast$coil$mean
+dt$l.coil[(R+1):(R+P)] <- for3$forecast$coil$lower
+dt$u.coil[(R+1):(R+P)] <- for3$forecast$coil$upper
+
+dt$f.infl[(R+1):(R+P)] <- for3$forecast$infl$mean
+dt$l.infl[(R+1):(R+P)] <- for3$forecast$infl$lower
+dt$u.infl[(R+1):(R+P)] <- for3$forecast$infl$upper
+
+gg1 <- ggplot(dt,aes(x=date))+
+  geom_ribbon(aes(ymin=l.coil,ymax=u.coil),fill="coral",alpha=.2)+
+  geom_line(aes(y=coil),linewidth=.6,color="dimgray",na.rm=T)+
+  geom_line(data=dt[date>=h1],aes(y=coil),linewidth=.6,color="gray",na.rm=T)+
+  geom_line(aes(y=l.coil),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=u.coil),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=f.coil),linetype=5,linewidth=.6,color="coral",na.rm=T)+
+  labs(y="",x="Year",title="Crude Oil Prices - WTI",subtitle="($/bbl)")+
+  coord_cartesian(ylim=c(0,150),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+
+gg2 <- ggplot(dt,aes(x=date))+
+  geom_ribbon(aes(ymin=l.infl,ymax=u.infl),fill="coral",alpha=.2)+
+  geom_line(aes(y=infl),linewidth=.6,color="dimgray",na.rm=T)+
+  geom_line(data=dt[date>=h1],aes(y=infl),linewidth=.6,color="gray",na.rm=T)+
+  geom_line(aes(y=l.infl),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=u.infl),linetype=2,linewidth=.4,color="coral",na.rm=T)+
+  geom_line(aes(y=f.infl),linetype=5,linewidth=.6,color="coral",na.rm=T)+
+  labs(y="",x="Year",title="5-yr Expected Inflation",subtitle="(%)")+
+  coord_cartesian(ylim=c(-2,4),xlim=c(as.Date("2004-01-01"),as.Date("2022-12-31")))+
+  theme_eg()
+    
+gg <- plot_grid(gg1,gg2,align="hv",axis="tb",nrow=1)
+
+ggsave("figures/lecture10/forecasts.png",gg,width=6.5,height=6.5*8/16,dpi="retina",device="png")
   
   
-  
-  # Testing in-sample Granger causality
+  # Forecasting infl rate
   
   .right-column[
     ```{r echo=FALSE, message=FALSE, cache=FALSE}
     
-    ar31 <- lm(funds~funds1+funds2+funds3,data=dt)
-    ar32 <- lm(inflation~inflation1+inflation2+inflation3,data=dt)
-    
-    f31 <- anova(var31,ar31)
-    f32 <- anova(var32,ar32)
-    
-    out <- rbind(f31[2,c(5:6)],f32[2,c(5:6)])
-    rownames(out) <- c("Inflation Rate GC Interest Rate","Interest Rate GC Inflation Rate")
-    
-    kable(out, booktabs = TRUE,digits=3,align="l", table.attr='class="myTable"',row.names = T)
-    
-    ```
-  ]
-
----
-  
-  
-  
-  
-  # Forecasting interest rate
-  
-  .right-column[
-    ```{r echo=FALSE, message=FALSE, cache=FALSE}
-    est_end <- paste0(substr(dt$date[round(.75*length(dt$date))],1,4),"-12-01")
-    R <- which(dt$date==est_end)
-    P <- nrow(dt)-R
-    
-    ts_dt <- ts(dt[1:R,.(funds,inflation)],start=c(as.numeric(substr(dt$date[1],1,4)),as.numeric(substr(dt$date[1],6,7))),frequency = 12)
-    
-    var3 <- VAR(ts_dt,p=3,type="const")
-    
-    for3 <- forecast(var3,level=95,h=P)
-    
-    
-    dt$f.funds <- NA
-    dt$l.funds <- NA
-    dt$u.funds <- NA
-    
-    dt$f.inflation <- NA
-    dt$l.inflation <- NA
-    dt$u.inflation <- NA
-    
-    dt$f.funds[(R+1):(R+P)] <- for3$forecast$funds$mean
-    dt$l.funds[(R+1):(R+P)] <- for3$forecast$funds$lower
-    dt$u.funds[(R+1):(R+P)] <- for3$forecast$funds$upper
-    
-    dt$f.inflation[(R+1):(R+P)] <- for3$forecast$inflation$mean
-    dt$l.inflation[(R+1):(R+P)] <- for3$forecast$inflation$lower
-    dt$u.inflation[(R+1):(R+P)] <- for3$forecast$inflation$upper
-    
-    ggplot(dt,aes(x=date,y=funds))+
-      geom_ribbon(aes(ymin=l.funds,ymax=u.funds),fill="coral",alpha=.2)+
-      geom_line(color="powderblue",size=.8)+
-      geom_line(data=dt[date>as.Date(est_end)],color="gray",size=.8)+
-      geom_line(aes(y=f.funds),color="coral",size=.8,linetype=5,na.rm=T)+
-      labs(x="Year",y="Federal Funds Effective Rate",caption="retrieved from FRED, Federal Reserve Bank of St. Louis\nhttps://fred.stlouisfed.org/series/FEDFUNDS")+
-      theme_classic()+
-      theme(axis.title = element_text(size=22),axis.text = element_text(size=18))
-    ```
-  ]
-
----
-  
-  
-  # Forecasting inflation rate
-  
-  .right-column[
-    ```{r echo=FALSE, message=FALSE, cache=FALSE}
-    ggplot(dt,aes(x=date,y=inflation))+
-      geom_ribbon(aes(ymin=l.inflation,ymax=u.inflation),fill="coral",alpha=.2)+
-      geom_line(color="powderblue",size=.8)+
-      geom_line(data=dt[date>as.Date(est_end)],color="gray",size=.8)+
-      geom_line(aes(y=f.inflation),color="coral",size=.8,linetype=5,na.rm=T)+
-      labs(x="Year",y="Year-Over-Year Inflation",caption="retrieved from FRED, Federal Reserve Bank of St. Louis\nhttps://fred.stlouisfed.org/series/CPIAUCNS")+
-      theme_classic()+
-      theme(axis.title = element_text(size=22),axis.text = element_text(size=18))
     ```
   ]
 
